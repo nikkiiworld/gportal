5acd7416667af7476c9565ed37e9e7415556c134
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-10-03 23:47:19 +0200

    Update short_content.js

--- scripts/short_content/short_content.js
+++ scripts/short_content/short_content.js
@@ -1,46 +1,41 @@
-//(function($) {
-
-        var roviditettBejegyzesek = function (max = 250, readMoreText = "[..]", firstPost = false){
+function roviditettBejegyzesek (max = 250, readMoreText = "[..]", firstPost = false){
         
-            if( window.location.search.indexOf('postid') ===  -1 ){
+    if( window.location.search.indexOf('postid') ===  -1 ){
 
-                var readmore = '<a class="readmore" href="#">'+readMoreText+'</a>';
-                if($('.post-content').length > 0){
+        var readmore = '<a class="readmore" href="#">'+readMoreText+'</a>';
+        if($('.post-content').length > 0){
 
-                   $('.post-content').each(function(i){
-                       if( firstPost === true && i === 0 ){
-                           i++;
-                       }else{
-                            var title = $(this).closest('h2');
-                            if(title.parent().is('a')){
-                               readmore.attr('href',title.parent().attr('href'));
-                            }else{
-                               readmore.attr('href', '#');      
-                            }
-                            $(this).html('<p>'+$('.post-content').text().substring(0,max) + readmore + '</p>');
-                       }
+           $('.post-content').each(function(i){
+               if( firstPost === true && i === 0 ){
+                   i++;
+               }else{
+                    var title = $(this).closest('h2');
+                    if(title.parent().is('a')){
+                       readmore.attr('href',title.parent().attr('href'));
+                    }else{
+                       readmore.attr('href', '#');      
+                    }
+                    $(this).html('<p>'+$('.post-content').text().substring(0,max) + readmore + '</p>');
+               }
 
-                   });
+           });
+        }else{
+
+           $('div[id^="post_"]').each(function(i){
+                if(firstPost === true && i === 0){
+                    i++;
                 }else{
-
-                   $('div[id^="post_"]').each(function(i){
-                        if(firstPost === true && i === 0){
-                            i++;
-                        }else{
-                            var title = $(this).closest('h2');
-                            if(title.parent().is('a')){
-                               readmore.attr('href',title.parent().attr('href'));
-                            }else{
-                               readmore.attr('href', '#');      
-                            }
-                            $(this).html('<p>'+$('div:not([attr_all])').text().substring(0,max) + readmore + '</p>');
-                        }
-                   });
-
+                    var title = $(this).closest('h2');
+                    if(title.parent().is('a')){
+                       readmore.attr('href',title.parent().attr('href'));
+                    }else{
+                       readmore.attr('href', '#');      
+                    }
+                    $(this).html('<p>'+$('div:not([attr_all])').text().substring(0,max) + readmore + '</p>');
                 }
-
-            }
+           });
 
         }
-        
-//})(jQuery); 
+
+    }
+}
