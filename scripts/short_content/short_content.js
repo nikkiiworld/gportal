e160b75c6ca7ebe2e3b6833ffa78210b4f7e38b0
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-10-04 00:40:58 +0200

    Update short_content.js

--- scripts/short_content/short_content.js
+++ scripts/short_content/short_content.js
@@ -9,9 +9,9 @@ function roviditettBejegyzesek (max = 250, readMoreText = "[..]", firstPost = fa
                if( firstPost === true && i === 0 ){
                    i++;
                }else{
-                    var title = $(this).closest('h2');
-                    if(title.parent().is('a')){
-                       var readmore = '<a class="readmore" href="'+title.parent().attr('href')+'">'+readMoreText+'</a>';
+                    var link = $(this).parent().children('a');
+                    if( link !== undefined ){
+                       var readmore = '<a class="readmore" href="'+ link.attr('href')+'">'+readMoreText+'</a>';
                     }else{
                        var readmore = '<span class="readmore">'+readMoreText+'</span>';
                     }
