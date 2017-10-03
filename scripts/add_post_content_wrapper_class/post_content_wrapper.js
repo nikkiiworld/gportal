7679557114abb38f9549b4556ec73972de56de45
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-10-04 00:06:08 +0200

    Update post_content_wrapper.js

--- scripts/add_post_content_wrapper_class/post_content_wrapper.js
+++ scripts/add_post_content_wrapper_class/post_content_wrapper.js
@@ -2,11 +2,12 @@
     $(document).ready(function(){
         var posts = $('div[id^="post_"]');
         posts.each(function(){
-            var div = $(this).children('div:not([attr_all])').find(function(div){
+            console.log($(this).children('div:not([attr_all])'));
+            /*var div = $(this).children('div:not([attr_all])').find(function(div){
               return $(div).attr('style') === null && $(div).attr('class') === null;
             });
             console.log(div);
-            $(div).addClass("post-content");
+            $(div).addClass("post-content");*/
         });
     });
 })(jQuery)
