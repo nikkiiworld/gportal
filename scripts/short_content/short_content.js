e7377346a5e3020579a6fc3e61acd8fe5a7f35c2
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-10-03 23:36:09 +0200

    Update short_content.js

--- scripts/short_content/short_content.js
+++ scripts/short_content/short_content.js
@@ -1,46 +1,46 @@
-(function(){
-    function roviditettBejegyzesek(max = 250, firstPost = false){
+(function($){
 
-        if(Number.isInteger(max) && max > 0){
-            var postsLength = $('div[id^="post_"]').length;
-            if( postsLength > 1){
-                var divs = {};
+        function roviditettBejegyzesek(max = 250, readMoreText = "[..]", firstPost = false){
+        
+            if( window.location.search.indexOf('postid') ===  -1 ){
+            
+                var readmore = '<a class="readmore" href="#">'+readMoreText+'</a>';
+                if($('.post-content').length > 0){
 
-                if(firstPost === false){
-                    divs = $('div[id^="post_"]:not(:first-of-type)').find('div:not([attr_all])');
+                   $('.post-content').each(function(i){
+                       if( firstPost === true && i === 0 ){
+                           i++;
+                       }else{
+                            var title = $(this).closest('h2');
+                            if(title.parent().is('a'){
+                               readmore.attr('href',title.parent().attr('href'));
+                            }else{
+                               readmore.attr('href', '#');      
+                            }
+                            $(this).html('<p>'+$('.post-content').text().substring(0,max) + readmore + '</p>');
+                       }
+                       
+                   });
                 }else{
-                    divs = $('div[id^="post_"]').find('div:not([attr_all])');
-                }
-
-                var i = firstPost === false ? 1 : 0;
-
-                for(i; i<divs.length; i++){
-
-                    if($(divs[i]).attr('class') === undefined && $(divs[i]).attr('id') === undefined && $(divs[i]).attr('style') === undefined){
-                        var featimg = "";
-                        if($(divs[i]).closest('div[id^="post_"]').find('.featured').length > 0){
-                            featimg = $(divs[i]).closest('div[id^="post_"]').find('.featured')[0];
-                        }
-                        var txt = $(divs[i]).text();
-                        var more = '';
-
-                        if($(divs[i]).parent().find('.blogmorelink').length !== 0){
-                            $(divs[i]).parent().find('.blogmorelink').hide();
-                        }
-
-                        if(txt.length > max){
-                            more = '<p class="readmore"><a class="readmore-link" href="'+$(divs[i]).parent().find('h2').parent().attr('href')+'">Tovább olvas &rarr;</a></p>';
-                        }
-                        if(featimg !== null && featimg !== ""){
-                            $(divs[i]).addClass('post-content').html('<p>'+txt.substr(0,max) + '..</p> <img class="featured" src="'+$(featimg).attr('src')+'">'+more);
+                
+                   $('div[id^="post_"]').each(function(i){
+                        if(firstPost === true && i === 0){
+                            i++;
                         }else{
-                            $(divs[i]).addClass('post-content').html('<p>'+txt.substr(0,max) + '..</p>'+more);
+                            var title = $(this).closest('h2');
+                            if(title.parent().is('a'){
+                               readmore.attr('href',title.parent().attr('href'));
+                            }else{
+                               readmore.attr('href', '#');      
+                            }
+                            $(this).html('<p>'+$('div:not([attr_all])').text().substring(0,max) + readmore + '</p>');
                         }
-
-                    }
-
+                   });
+                    
                 }
+                
             }
-        }
+  
     }
+    
 })(jQuery);
