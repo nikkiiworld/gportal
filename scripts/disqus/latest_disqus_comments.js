2e0567f8825224e0706c9635d299ca7f1ccfdec9
nikkiiworld <30777505+nikkiiworld@users.noreply.github.com>
2017-10-04 00:54:36 +0200

    Update latest_disqus_comments.js

--- scripts/disqus/latest_disqus_comments.js
+++ scripts/disqus/latest_disqus_comments.js
@@ -1,36 +1,33 @@
-(function($, moment){
-    $(document).ready(function(){
-        var latestComments = function(selector, forum, limit, apiKey) {
-            if(!selector || $(selector).length === 0){
-                selector = $('.lastcomments');
+function latestDisqusComments (selector, forum, limit, apiKey) {
+    
+    if(!selector || $(selector).length === 0){
+        selector = $('.lastcomments');
+    }
+    $.get("https://disqus.com/api/3.0/forums/listPosts.json?forum="+forum+"&limit="+limit+"&related=thread&api_key="+apiKey, function(res, code) {
+        if(res.code === 0) {
+            var result = "";
+            for(var i=0, len=res.response.length; i<len; i++) {
+                var post = res.response[i];
+
+                var template = `
+                    <div class="latestcomments">
+                        <span class="comment_nev">
+                            <img class="comment_ikon" src="${post.author.avatar.small.permalink}">
+                            <a href="${post.author.profileUrl}">${post.author.name}</a>
+                        </span>
+                        <a href="${post.thread.link}">
+                            <span class="comment_text">
+                                ${ post.raw_message.length > 100 ?
+                    post.raw_message.substring(0,100) + '...' : post.raw_message }
+                            </span>
+                        </a>
+                        <span class="comment_date postRef">${moment.utc(post.createdAt).locale("hu").fromNow()}</span>
+                    </div>"   
+                `;
+                result += template;
+
             }
-            $.get("https://disqus.com/api/3.0/forums/listPosts.json?forum="+forum+"&limit="+limit+"&related=thread&api_key="+apiKey, function(res, code) {
-                if(res.code === 0) {
-                    var result = "";
-                    for(var i=0, len=res.response.length; i<len; i++) {
-                        var post = res.response[i];
-
-                        var template = `
-                            <div class="latestcomments">
-                                <span class="comment_nev">
-                                    <img class="comment_ikon" src="${post.author.avatar.small.permalink}">
-                                    <a href="${post.author.profileUrl}">${post.author.name}</a>
-                                </span>
-                                <a href="${post.thread.link}">
-                                    <span class="comment_text">
-                                        ${ post.raw_message.length > 100 ?
-                            post.raw_message.substring(0,100) + '...' : post.raw_message }
-                                    </span>
-                                </a>
-                                <span class="comment_date postRef">${moment.utc(post.createdAt).locale("hu").fromNow()}</span>
-                            </div>"   
-                        `;
-                        result += template;
-
-                    }
-                    selector.html(result);
-                }
-            });
-        }();
+            selector.html(result);
+        }
     });
-})(jQuery, moment);
+}
