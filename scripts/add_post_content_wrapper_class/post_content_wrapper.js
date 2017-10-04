b0bfe740e63979d539054ed5b4f79b7d514411e2
zk3rpq <k.nikolett17@gmail.com>
2017-10-04 03:08:12 +0200

    infinity scroll functions

--- scripts/add_post_content_wrapper_class/post_content_wrapper.js
+++ scripts/add_post_content_wrapper_class/post_content_wrapper.js
@@ -1,11 +1,13 @@
-$(document).ready(function(){
-    var addPostContentClass = (function (){
-        $('div[id^="post_"]').each(function(){
-            $(this).children('div:not([attr_all])').each(function(){
-                if( $(this).attr('style') === undefined && $(this).attr('class') === undefined ){
-                    $(this).addClass('post-content');
-                }
-            });
+function addPostContentClass(){
+    $('div[id^="post_"]').each(function(){
+        $(this).children('div:not([attr_all])').each(function(){
+            if( $(this).attr('style') === undefined && $(this).attr('class') === undefined ){
+                $(this).addClass('post-content');
+            }
         });
-    }());
+    });
+}
+
+$(document).ready(function(){
+    addPostContentClass();
 });
