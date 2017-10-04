b97d17750823d638e5fe25f51d8c91aa26f2c7e5
zk3rpq <k.nikolett17@gmail.com>
2017-10-04 03:00:11 +0200

    infinity scroll functions

--- scripts/add_post_content_wrapper_class/post_content_wrapper.js
+++ scripts/add_post_content_wrapper_class/post_content_wrapper.js
@@ -1,13 +1,11 @@
-(function($){
-    $(document).ready(function(){
-        var addPostContentClass = function (){
-            $('div[id^="post_"]').each(function(){
-                $(this).children('div:not([attr_all])').each(function(){
-                    if( $(this).attr('style') === undefined && $(this).attr('class') === undefined ){
-                        $(this).addClass('post-content');
-                    }
-                });
+$(document).ready(function(){
+    var addPostContentClass = (function (){
+        $('div[id^="post_"]').each(function(){
+            $(this).children('div:not([attr_all])').each(function(){
+                if( $(this).attr('style') === undefined && $(this).attr('class') === undefined ){
+                    $(this).addClass('post-content');
+                }
             });
-        }();
-    });
-})(jQuery);
+        });
+    })();
+});
