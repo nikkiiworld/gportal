ae662ec83b1b58009c25ce0edca2f9fdcb04ff28
nikkiiworld <dgMSN79j>
2018-01-20 19:24:46 +0100

    add blomodul class

--- scripts/blogmodul-class/blogmodul-class.js
+++ scripts/blogmodul-class/blogmodul-class.js
@@ -1,6 +1,6 @@
 function blogmodulClass(){
     if($('div[id^="post_"]').length > 0 ){
-        var table = $('div[id^="post_"]:first-of-type').closest('table.module');
+        var table = $('div[id^="post_"]').first().closest('table.module');
         $(table).addClass('blogmodul');
         if(window.location.search.indexOf('postid') !==  -1){
             $(table).addClass('full-content');
