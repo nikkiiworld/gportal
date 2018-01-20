275776e6237739970b44b396cb5ee14dc3be37d6
nikkiiworld <dgMSN79j>
2018-01-20 19:05:30 +0100

    add blomodul class

--- scripts/blogmodul-class/blogmodul-class.js
+++ scripts/blogmodul-class/blogmodul-class.js
@@ -1,5 +1,5 @@
 function blogmodulClass(){
     if($('div[id^="post_"]').length > 0 && window.location.search.indexOf('postid') ===  -1 ){
-        $('div[id^="post_"]').closest('.modbody').addclass('blogmodul');
+        $('div[id^="post_"]:first-of-type').closest('.modbody').addClass('blogmodul');
     }
 }
