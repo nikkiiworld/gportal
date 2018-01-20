e7284a1efccdcb09fb57f430aeb35a1420a3a765
nikkiiworld <dgMSN79j>
2018-01-20 19:13:00 +0100

    add blomodul class

--- scripts/blogmodul-class/blogmodul-class.js
+++ scripts/blogmodul-class/blogmodul-class.js
@@ -1,6 +1,9 @@
 function blogmodulClass(){
-    if($('div[id^="post_"]').length > 0 && window.location.search.indexOf('postid') ===  -1 ){
+    if($('div[id^="post_"]').length > 0 ){
         var table = $('div[id^="post_"]:first-of-type').closest('table.module');
         $(table).addClass('blogmodul');
+        if(window.location.search.indexOf('postid') !==  -1){
+            $(table).addClass('full-content');
+        }
     }
 }
