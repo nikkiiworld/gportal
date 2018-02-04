54ee62f9db8ec0b9146e402a2df42bab7944618a
nikkiiworld <dgMSN79j>
2018-02-04 12:56:41 +0100

    Előző/Következő felirat felcserélése Hírek/Cikkek modulban

---
+++ scripts/change_prev_next_text.js
@@ -0,0 +1,20 @@
+function changePrevNext(){
+    $('.txtnormal').each(function(){
+
+        var that = $(this);
+        var txt = that.text();
+
+        if($(this).has('a').length === 1){
+            that = $(this).find('a');
+            txt = that.text();
+        }
+
+        if( /^Következő (\d+) cikk$/.test(txt) === true ){
+            txt = txt.replace(/Következő/gi, "Előző");
+        }else if( /^Előző (\d+) cikk$/.test(txt) === true ){
+            txt = txt.replace(/Előző/gi, "Következő");
+        }
+
+        $(that).text(txt);
+    });
+}
